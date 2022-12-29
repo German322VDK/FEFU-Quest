770183c5c67e77621dfa2a9ac71082786bd6278d
гера шлычков <germean322@gmail.com>
2022-12-29 14:21:56 +1000

    Рефакторинг

---
+++ FEFU-Quest/Controllers/EventsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FEFU_Quest.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dormitory()
+        {
+            return View();
+        }
+
+        public IActionResult Fefu()
+        {
+            return View();
+        }
+    }
+}
